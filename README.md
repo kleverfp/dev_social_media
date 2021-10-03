7301449602d5f263684e6c611cc095372b53c910
Klever Ferreira <kleverfp@gmail.com>
2021-10-03 11:26:11 -0300

    add jwt and save user

---
+++ README.md
@@ -0,0 +1,6 @@
+# Dev social Media
+
+A social media to developers. 
+This project was developed using MERN stack.
+
+### Dependecies
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const { body, validationResult } = require('express-validator');
-
+const User = require('../../models/User');
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 
 
 router.get('/',(req,res)=>{
@@ -13,14 +17,52 @@ router.post('/',[
     body('email','please include a valid email').isEmail(),
     body('password','please enter a password with 6 or more characters').isLength({min:6})
 
-],(req,res)=>{
+],async (req,res)=>{
     const erros = validationResult(req);
-    if(!erros.isEmpty()){
+    if(!erros.isEmpty())
         return res.status(400).json({erros:erros.array()});
-    }
+    
 
-    console.log(req.body);
-    res.json({msg:"ok"});
+    const {name,email,password} = req.body;
+
+    try{
+        let user = await User.findOne({email});
+        
+        if(user)
+            return res.status(400).json({msg:'email already exists'});
+        
+        const avatar = gravatar.url(email,{
+            s:'200',
+            r:'pg',
+            d:'mm'
+        });
+        user= new User({
+            name,
+            email,
+            avatar,
+            password
+        });
+        
+        const salt = await bcrypt.genSalt(10);
+        user.password = await bcrypt.hash(password,salt);
+        await user.save();
+
+        const payload ={
+            user:{
+                id:user.id
+            }
+        }
+
+        jwt.sign(payload,config.get('jwtSecret'),{
+            expiresIn:3600
+        },(err,token)=>{
+            if(err) throw err;
+            res.json({token});
+        });
+
+    }catch(err){
+        console.error(err.message);
+    }
    
 })
 
