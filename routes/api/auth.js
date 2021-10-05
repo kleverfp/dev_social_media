c8127e476af0afe95b4a1126792303cd7a754424
Klever Ferreira <kleverfp@gmail.com>
2021-10-05 19:38:16 -0300

    add auth middleware

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth');
+const User = require('../../models/User');
 
 
-
-
-router.get('/',(req,res)=>{
-    res.json({msg:"auth route"});
+router.get('/',auth,async (req,res)=>{
+    try {
+        const user = User.findById(req.user.id).select('-password');
+        if(user)
+            res.send(200).json(user);
+    } catch (err) {
+        console.log(err.message);
+        res.send(500).json({msg:'server error'});
+    }
 });
 
 
