f0397212035605ce70dc06b71a445fad1170eb57
Klever Ferreira <kleverfp@gmail.com>
2021-11-02 11:23:07 -0300

    change social media name

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -34,7 +34,7 @@ const Navbar=({auth:{isAuthenticated,loading},logout})=>{
     return (
         <nav className="navbar bg-dark">
             <h1>
-            <Link to="/"><i className="fas fa-code"></i> DevConnector</Link>
+            <Link to="/"><i className="fas fa-code"></i> Devbook</Link>
             </h1>
             {!loading && (<Fragment>{isAuthenticated ? authLinks:guessLink}</Fragment>)}
         </nav>
