396fb4faecd3e970aebd17602ecb34d0e95c0855
Klever Ferreira <kleverfp@gmail.com>
2021-11-02 00:45:33 -0300

    create addExprince form

--- client/src/App.js
+++ client/src/App.js
@@ -15,6 +15,7 @@ import store from './store';
 import PrivateRoute from './components/routing/PrivateRoute';
 import CreateProfile from './components/profile-forms/CreateProfile';
 import EditProfiles from './components/profile-forms/EditProfile';
+import AddExperience from './components/profile-forms/AddExperience';
 
 if(localStorage.token)
 setAuthToken(localStorage.token);
@@ -40,6 +41,7 @@ useEffect(()=>{
                   <PrivateRoute exact path="/dashboard" component={Dashboard}/>
                   <PrivateRoute exact path="/create-profile" component={CreateProfile}/>
                   <PrivateRoute exact path="/edit-profile" component={EditProfiles}/>
+                  <PrivateRoute exact path="/add-experience" component={AddExperience}/>
               </Switch>
           </section>
         </Fragment>
